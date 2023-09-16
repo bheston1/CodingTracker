419a8e42bc6df7a697b81109152d25c69198b861
bheston1 <bheston721@gmail.com>
2023-09-16 00:02:12 -0400

    added database creation

---
+++ CodingTracker/DatabaseFunctions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Specialized;
+using Microsoft.Data.Sqlite;
+
+namespace CodingTracker
+{
+    internal class DatabaseFunctions
+    {
+        internal static void CreateDB()
+        {
+            string connectionString = ConfigurationManager.AppSettings.Get("ConnectionString");
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCommand = connection.CreateCommand();
+                tableCommand.CommandText = "CREATE TABLE IF NOT EXISTS Sessions (Id INTEGER PRIMARY KEY AUTOINCREMENT, Date TEXT, Start TEXT, End TEXT, Duration TEXT)";
+                tableCommand.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+    }
+}
